93976d2660e857ee23147eb53482c9dd6fd73a22
Grzegorz <grzechtl@gmail.com>
2022-08-01 22:49:01 +0200

    Maximum element

--- src/main/python/algo/maximum_element.py
+++ src/main/python/algo/maximum_element.py
@@ -1,33 +1,35 @@
-def getMax(operations):
+def get_max(operations):
     # Write your code here
 
+    out = []
     stack = []
 
     for element in operations:
-        print(element)
 
         if element.startswith('1'):
-            splitedElementOne = element.split(' ')
-            operationOneValue = splitedElementOne[1]
-
-            print('operation: 1, value ', operationOneValue)
-            stack.append(operationOneValue)
+            split_element_one = element.split(' ')
+            operation_one_value = int(split_element_one[1])
+            newElem = stack[-1] if stack and stack != [] and operation_one_value < stack[-1] else operation_one_value
+            stack.append(newElem)
 
         elif element.startswith('2'):
             stack.pop()
 
         else:
-            print()
+            out.append(stack[-1])
+
+    return out
 
 
+out = get_max(['1 97',
+         '2',
+         '1 20',
+         '2',
+         '1 26',
+         '1 20',
+         '2',
+         '3',
+         '1 91',
+         '3'])
 
-getMax(['1 97',
-        '2',
-        '1 20',
-        '2',
-        '1 26',
-        '1 20',
-        '2',
-        '3',
-        '1 91',
-        '3'])
+print(out)
